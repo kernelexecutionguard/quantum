08290dd4f5dca620dbaba77ac2e970c72dd3c359
Eldar <eldar.aliyef2136@gmail.com>
2026-02-19 18:03:03 +0100

    Update EH-Main.lua

--- EH-Main.lua
+++ EH-Main.lua
@@ -1,1 +1,2 @@
-getgenv().Identifier = "eldarx-main" loadstring(game:HttpGet("https://api.nexar.to/scripts/Loader.lua"))()
+getgenv().Identifier = "eldarx-eh" loadstring(game:HttpGet("https://api.nexar.to/scripts/Loader.lua"))()
+
