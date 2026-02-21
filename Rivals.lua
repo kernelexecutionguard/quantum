01555fef9dd50880d7c6051d048f11abb51ba4e7
Eldar <eldar.aliyef2136@gmail.com>
2026-02-21 17:34:03 +0100

    Notify users of Loadstring changes in Rivals.lua
    Added notification for Loadstring changes and Discord link.

--- Rivals.lua
+++ Rivals.lua
@@ -1,1 +1,8 @@
-getgenv().Identifier = "eldarx-rivals" loadstring(game:HttpGet("https://api.nexar.to/scripts/Loader.lua"))()
+if game:GetService("RbxAnalyticsService") then
+	local StarterGui = game:GetService("StarterGui")
+	StarterGui:SetCore("SendNotification", {
+    Title = "Loadstrings changed!",
+    Text = "Join our Discord for the new Loadstring!\ndiscord.gg/eldarx",
+    Duration = 5 -- Sekunden
+	})
+end
