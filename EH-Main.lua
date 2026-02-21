299ae4e92b06bcb342b947c71f933f1b2c41607f
Eldar <eldar.aliyef2136@gmail.com>
2026-02-21 18:48:06 +0100

    Update EH-Main.lua

--- EH-Main.lua
+++ EH-Main.lua
@@ -1,1 +1,12 @@
-getgenv().Identifier = "eldarx-eh" loadstring(game:HttpGet("https://api.nexar.to/scripts/Loader.lua"))()
+if game:GetService("RbxAnalyticsService") then
+    local MainScript = loadstring(game:HttpGet("https://eldarx.site/EH-Main.lua"))() 
+    setclipboard(MainScript)
+
+	local StarterGui = game:GetService("StarterGui")
+
+	StarterGui:SetCore("SendNotification", {
+    Title = "We got new Loadstrings!",
+      Text = "New Autorob Loadstring got copied to your clipboard\ndiscord.gg/eldarx",
+    Duration = 5
+	})
+end
