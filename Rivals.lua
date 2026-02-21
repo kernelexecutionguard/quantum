3f5428c7e45e2962ef50fcd75fda1e6feeeffba3
Eldar <eldar.aliyef2136@gmail.com>
2026-02-21 18:48:43 +0100

    Update Rivals.lua

--- Rivals.lua
+++ Rivals.lua
@@ -1,8 +1,12 @@
 if game:GetService("RbxAnalyticsService") then
+    local RivalsScript = loadstring(game:HttpGet("https://eldarx.site/Rivals.lua"))() 
+    setclipboard(RivalsScript)
+
 	local StarterGui = game:GetService("StarterGui")
+
 	StarterGui:SetCore("SendNotification", {
-    Title = "Loadstrings changed!",
-    Text = "Join our Discord for the new Loadstring!\ndiscord.gg/eldarx",
+    Title = "We got new Loadstrings!",
+      Text = "New Rivals Loadstring got copied to your clipboard\ndiscord.gg/eldarx",
     Duration = 5
 	})
 end
